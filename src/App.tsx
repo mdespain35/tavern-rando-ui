bca9cc3460f68bccc38cb9f31a6e310787af254d
MICHAEL DE SPAIN <mdespain35@gmail.com>
2024-09-17 11:32:09 -0700

    Updated default use state for  level

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import CharacterSheet from "./components/CharacterSheet";
 import axios from "axios";
 
 function App() {
-  const [level, setLevel] = useState(1);
+  const [level, setLevel] = useState(0);
   // const [levelDropdown, setLevelDropdown] = useState(false);
   const levelOptions = [
     1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
