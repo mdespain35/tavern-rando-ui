18bd1466c2bd215e2e62883febd29406807dfce8
MICHAEL DE SPAIN <mdespain35@gmail.com>
2024-09-17 11:31:42 -0700

    Updated styling for ability scores

--- src/components/AbilityScore.tsx
+++ src/components/AbilityScore.tsx
@@ -6,10 +6,10 @@ interface AbilityScoreProps {
 
 const AbilityScore = ({ name, score, modifier }: AbilityScoreProps) => {
   return (
-    <div className="border border-dark p-3 mx-auto">
+    <div className="border border-dark p-3 ability-scores mx-auto">
       <h5 className="border-bottom border-dark p-2 mx-auto">{name}</h5>
-      <p className="h3">{modifier > 0 ? "+" + modifier : modifier}</p>
-      <p className="border rounded-circle mt-n2">{score}</p>
+      <p className="h2">{modifier > 0 ? "+" + modifier : modifier}</p>
+      <p className="border rounded-circle mt-4 mb-0 p-1">{score}</p>
     </div>
   );
 };
